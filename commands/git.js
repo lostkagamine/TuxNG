3d903b85e32b9fff99c95fd3d78769ab44303dc3
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-04-25 20:11:58 +0100

    added nifty things

---
+++ commands/git.js
@@ -0,0 +1,21 @@
+const superagent = require('superagent')
+
+module.exports = {
+    name: 'git',
+    ownerOnly: true,
+    code: async (ctx, args) => {
+        let process = require('child_process').spawnSync('git', args)
+        let output = `\`\`\`${process.stdout.toString()}\n${process.stderr.toString()}\`\`\`Exited with code ${process.status}, PID ${process.pid}`
+        let outputPost = `- Exit code: ${process.status}\n- PID: ${process.pid}\n\n- Stdout:\n\n${process.stdout.toString()}\n- Stderr:\n\n${process.stderr.toString()}`
+        if (output.length > 2000) {
+            superagent.post('https://hastebin.com/documents')
+                .type('text/plain')
+                .send(outputPost)
+                .then(a => {
+                    ctx.send(`https://hastebin.com/${a.body.key}`)
+                })
+        } else {
+            await ctx.send(output)
+        }
+    }
+}
---
+++ commands/sh.js
@@ -0,0 +1,28 @@
+const superagent = require('superagent')
+
+module.exports = {
+    name: 'sh',
+    ownerOnly: true,
+    code: async (ctx, args) => {
+        let process = require('child_process').spawnSync(args.shift(), args)
+        let codes = {
+            ENOENT: 'File not found.'
+        }
+        if (process.error) {
+            await ctx.send(`${process.error.code}: ${codes[process.error.code]}`)
+            return
+        }
+        let output = `\`\`\`${process.stdout.toString()}\n${process.stderr.toString()}\`\`\`Exited with code ${process.status}, PID ${process.pid}`
+        let outputPost = `- Exit code: ${process.status}\n- PID: ${process.pid}\n\n- Stdout:\n\n${process.stdout.toString()}\n- Stderr:\n\n${process.stderr.toString()}`
+        if (output.length > 2000) {
+            superagent.post('https://hastebin.com/documents')
+                .type('text/plain')
+                .send(outputPost)
+                .then(a => {
+                    ctx.send(`https://hastebin.com/${a.body.key}`)
+                })
+        } else {
+            await ctx.send(output)
+        }
+    }
+}
